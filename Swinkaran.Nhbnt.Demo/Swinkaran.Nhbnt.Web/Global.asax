5dba0ac9202b7b686fcc2df25c7299c36607d16c
swinkaran <swinkaran@hotmail.com>
2017-03-06 15:43:51 +1100

    Initial project setup
    Initial project setup with the following projects
    - Data
    - MVC web site
    - Model

---
+++ Swinkaran.Nhbnt.Demo/Swinkaran.Nhbnt.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Swinkaran.Nhbnt.Web.MvcApplication" Language="C#" %>
---
+++ Swinkaran.Nhbnt.Demo/Swinkaran.Nhbnt.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Swinkaran.Nhbnt.Web.Startup))]
+namespace Swinkaran.Nhbnt.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
