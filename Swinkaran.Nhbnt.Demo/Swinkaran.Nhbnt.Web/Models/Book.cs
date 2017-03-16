84d5e1026c259c53288ff036ea08ff93193b4786
Swinkaran <swinkaran@hotmail.com>
2017-03-16 23:05:48 +1100

    Setting up the NHibernate files
    - NHibernate mapping files created
    - Domain model created
    - DB scipt included

---
+++ Swinkaran.Nhbnt.Demo/Swinkaran.Nhbnt.Web/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swinkaran.Nhbnt.Web.Models
+{
+    public class Book
+    {
+        public virtual long Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Author { get; set; }
+        public virtual DateTime PublishedOn { get; set; }
+        public virtual string Genre { get; set; }
+        public virtual decimal Price { get; set; }
+    }
+}
