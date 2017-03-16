154413a941db61ad2741e6fed40351ba71eb37c7
Swinkaran <swinkaran@hotmail.com>
2017-03-16 23:41:03 +1100

    Setting the NHibernate connection
    Setting the NHibernate connection

--- Swinkaran.Nhbnt.Demo/Swinkaran.Nhbnt.Web/Controllers/HomeController.cs
+++ Swinkaran.Nhbnt.Demo/Swinkaran.Nhbnt.Web/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using NHibernate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Swinkaran.Nhbnt.Web.Models;
+using NHibernate.Linq;
+using Swinkaran.Nhbnt.Web;
 
 namespace Swinkaran.Nhbnt.Web.Controllers
 {
@@ -16,6 +20,12 @@ namespace Swinkaran.Nhbnt.Web.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+
+            using (ISession session = NHibernateSession.OpenSession())
+            {
+                var book = session.Query<Book>().ToList();
+                return View();
+            }
 
             return View();
         }
---
+++ Swinkaran.Nhbnt.Demo/Swinkaran.Nhbnt.Web/NHibernateSession.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swinkaran.Nhbnt.Web
+{
+    public class NHibernateSession
+    {
+        public static ISession OpenSession()
+        {
+            var configuration = new Configuration();
+            var configurationPath = HttpContext.Current.Server.MapPath(@"~\Models\hibernate.cfg.xml");
+            configuration.Configure(configurationPath);
+            var employeeConfigurationFile = HttpContext.Current.Server.MapPath(@"~\Models\Book.hbm.xml");
+            configuration.AddFile(employeeConfigurationFile);
+            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
